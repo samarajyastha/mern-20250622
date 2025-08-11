eb8c3623fdde1f28ec6592eccaf042039561d066
samarajya <samarajya@dzangolab.com>
2025-08-11 20:51:38 +0545

    js intro

---
+++ main.js
@@ -0,0 +1,12 @@
+/**
+ * 1. Variables: var, let, const
+ * 2. Data types: string, number, boolean, array, object
+ * 3. If-else statement, switch, if-else ladder
+ * 4. Loop: For, while
+ * 5. EcmaScript
+ * 6. Object and array destructuring
+ * 7. Spread operator
+ * 8. Template literals
+ * 9. Higher order array methods: map, filter, sort, find
+ * 10. Async programming: callback, promises, async await
+ */
