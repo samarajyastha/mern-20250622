5e654d9c25a275f6b30d934aed5aad97a183d994
samarajya <samarajya@dzangolab.com>
2025-08-12 21:03:14 +0545

    dom manipulation

---
+++ README.md
@@ -0,0 +1,22 @@
+# JavaScript Document Object Model (DOM)
+
+- Window: The whole browser tab, with title, search bar, body 
+- Document: The body part of html in the browser
+
+<html>
+    <body>
+        <p></p>
+        <a href=""></a>
+    </body>
+</html>
+
+document
+|---- html
+|--------- body
+|------------- p
+|------------- a
+|---------------- href
+
+## DOM Manipulation (To change)
+
+- Updating the html content, styles using JS
--- main.js
+++ main.js
@@ -10,3 +10,48 @@
  * 9. Higher order array methods: map, filter, sort, find
  * 10. Async programming: callback, promises, async await
  */
+
+const title = document.getElementById("title");
+
+title.innerText = "Hello sita";
+title.style.color = "red";
+title.style.backgroundColor = "#dddddd";
+title.style.border = "1px solid black";
+
+const paragraph = document.querySelector("p");
+
+paragraph.innerText = "This is a paragraph";
+
+// Event listener
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", () => {
+  button.innerText = "clicked";
+  console.log("button is clicked");
+});
+
+button.addEventListener("dblclick", () => {
+  console.log("button is double clicked");
+
+  window.alert("This is alert");
+});
+
+button.addEventListener("mouseover", () => {
+  button.style.color = "red";
+});
+
+button.addEventListener("mouseleave", () => {
+  button.style.color = "green";
+});
+
+const listItems = ["Ram", "Sam", "Hari", "Sita", "Mohan"];
+
+const listElement = document.getElementById("list");
+
+for (let i = 0; i < listItems.length; i++) {
+  const listItem = document.createElement("li");
+
+  listItem.textContent = listItems[i];
+
+  listElement.appendChild(listItem);
+}
